cf93a2a0654d4c6f738f073a7ad0e8cdbd498c8c
yuyinws <lyc657508366@gmail.com>
2024-01-04 15:40:40 +0800

    feat: theme toggle animation

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,13 +4,12 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import 'uno.css'
 import './style.css'
+import CustomLayout from './CustomLayout.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    return h(CustomLayout)
   },
   enhanceApp() {
     // ...
